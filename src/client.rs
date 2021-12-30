47ca14bc2093ae81492d84bb354c1c8f1f3928ab
smsutherland <sagan@farthinghalearms.com>
2021-12-30 14:15:08 -0500

    store token in Client struct

--- src/client.rs
+++ src/client.rs
@@ -2,12 +2,14 @@ use crate::user::User;
 
 pub struct Client {
     user: User,
+    token: String,
 }
 
 impl Client {
     pub fn new(token: &str) -> Client {
         Client {
             user: User::from_bot_token(token),
+            token: String::from(token),
         }
     }
 }
