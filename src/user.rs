304853358cd3b4aebc58ac6cb543210bde2abe92
smsutherland <sagan@farthinghalearms.com>
2021-12-29 20:30:25 -0500

    change Client to store User struct

--- src/user.rs
+++ src/user.rs
@@ -1,7 +1,6 @@
 struct PartialServerIntegration; // TODO: Create PartialServerIntegration struct
 
 type Snowflake = u64; // TODO: Create Snowflake struct
-type ISO8601Timestamp = u64; // TODO: Research ISO8601 Timestamp
 pub struct User {
     id: Snowflake,
     username: String,
@@ -20,6 +19,43 @@ pub struct User {
     public_flags: Option<u64>,
 }
 
+impl User {
+    pub fn from_bot_token(token: &str) -> User {
+        let request_url = "https://discord.com/api/v9/users/@me";
+
+        let response = json::parse(
+            &ureq::get(request_url)
+                .set("Authorization", &format!("Bot {}", token))
+                .call()
+                .unwrap()
+                .into_string()
+                .unwrap(),
+        )
+        .unwrap();
+
+        User {
+            id: response["id"].as_str().unwrap().parse().unwrap(),
+            username: String::from(response["username"].as_str().unwrap()),
+            discriminator: response["discriminator"]
+                .as_str()
+                .map(String::from)
+                .unwrap(),
+            avatar: response["avatar"].as_str().map(String::from),
+            bot: response["bot"].as_bool(),
+            system: response["system"].as_bool(),
+            mfa_enabled: Some(response["mfa_enabled"].as_bool().unwrap()),
+            banner: response["banner"].as_str().map(String::from),
+            accent_color: response["accent_color"].as_u64(),
+            locale: Some(String::from(response["locale"].as_str().unwrap())),
+            verified: Some(response["verified"].as_bool().unwrap()),
+            email: response["email"].as_str().map(String::from),
+            flags: Some(response["flags"].as_u64().unwrap()),
+            premium_type: response["premium_type"].as_u8().map(PremiumType::from),
+            public_flags: Some(response["public_flags"].as_u64().unwrap()),
+        }
+    }
+}
+
 struct Connection {
     id: String,
     name: String,
@@ -36,6 +72,17 @@ enum PremiumType {
     None,
     NitroClassic,
     Nitro,
+}
+
+impl PremiumType {
+    fn from(num: u8) -> PremiumType {
+        match num {
+            0 => PremiumType::None,
+            1 => PremiumType::NitroClassic,
+            2 => PremiumType::Nitro,
+            _ => unreachable!(),
+        }
+    }
 }
 
 enum VisibilityType {
--- tests/bot.rs
+++ tests/bot.rs
@@ -3,5 +3,5 @@ use discord_bot::*;
 #[test]
 fn create_bot() {
     let bot_token = dotenv::var("BOT_TOKEN").unwrap();
-    let bot = client::Client::new(&bot_token).unwrap();
+    let bot = client::Client::new(&bot_token);
 }
